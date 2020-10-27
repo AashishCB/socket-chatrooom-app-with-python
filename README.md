389c94d7885f38159410bf8d3eac7fe47587a8f4
Aashish Chandra Bivek <aashish.2chandra@gmail.com>
2020-10-27 22:07:40 +0545

    wrote server.py

---
+++ README.md
@@ -0,0 +1,52 @@
+#### Incomplete
+
+# Under Progress
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Socket Chatroom-Creating chat applicatoin with sockets in python
+Sockets and the socket API are used to send messages across a network. They provide a form of inter-process communication (IPC).
+Python’s socket module provides an interface to the Berkeley sockets API.
+
+> The objective is to create a chat application having a server and multiple clients.
+
+### Prerequisites
+
+#### socket
+Python’s socket module provides an interface to the Berkeley sockets API.
+
+#### select
+
+> You must have python3 installed on your computer to run it.
+
+### to run the client and server .py file:
+
+1. Open cmd on the file stored location and:
+
+` $ python server.py `
+
+or 
+
+` $ python3 server.py `
+
+2. Open another cmd on the file stored location without closing the previous one:
+
+` $ python client.py `
+
+or 
+
+` $ python3 client.py `
---
+++ server.py
@@ -0,0 +1,90 @@
+import socket
+import select
+# to manage many connection, 
+# gives os level io capabilites. will work same on any platform
+
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 1234
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# When retrieving a socket option, or setting it, you specify the option name as well as the level. 
+# When level = SOL_SOCKET, the item will be searched for in the socket itself.
+# arguments -the thing we want to set, what we want to set of that thing, set the thing
+# 1 means true, here with above line will allow us to reconnect
+
+# port number keeps ticking up, so it's not in use, which we have to overcome
+
+
+# You want to run a socket server always on a specific port even after it is closed intentionally or unexpectedly.
+# This is useful in some cases where your client program always connects to that specific server port.
+# So, you don't need to change the server port.
+
+server_socket.bind((IP, PORT))
+
+server_socket.listen()
+# ------------------basic setup-----------------------------------------
+sockets_list = [server_socket]
+# list of clients, later we will port these clients with other clients
+
+clients = {}
+# client socket will be key and the user data will be the value
+
+
+def receive_message(client_socket):
+	try:
+		message_header = client_socket.recv(HEADER_LENGTH)
+
+		if not len(message_header):
+			return False
+
+		message_length = int(message_header.decode("utf-8").strip())
+		return {"header": message_header, "data": client_socket.recv(message_length)}
+		#  message_header containing size of message, data receving value at once depending on message length
+		# here we are not controlling the message receiving part by part,
+		# hopoing no one is going to send message of very large size.
+	except:
+		# only reaches here if somone broke their script
+		return False
+
+while True:
+	read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+	# we only care about read_sockets
+	# select.select takes three parameters
+	# first is readlist, second is write list and then error list
+	for notified_socket in read_sockets:
+		if notified_socket == server_socket:
+		# it means someone just connected and we need to accept the connection and handle for it.
+			client_socket, client_address = server_socket.accept()
+
+			user = receive_message(client_socket)
+			if user is False:
+			# someone just disconnected, so we will continue
+				continue
+
+			sockets_list.append(client_socket)
+
+			clients[client_socket] = user
+
+			print(f"Accepted new connection form {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
+
+		else:
+			message = receive_message(notified_socket)
+
+			if message is False:
+				print(f"Closed connection ffrom {clients[notified_socket]['data'].decode('utf-8')}")
+				sockets_list.remove(notified_socket)
+				del clients[notified_socket]
+				continue
+
+			user = clients[notified_socket]
+			print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+
+			for clients_socket in clients:
+				if clients_socket != clients:
+					clients_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+	for notified_socket in exception_sockets:
+		sockets_list.remove(notified_socket)
+		del clients[notified_socket]
